81e1210ebec936e5fdcd48731851f1edb0424fb7
Allison Dawes <allyssondawes@hotmail.com>
2019-07-11 15:37:03 -0300

    cadastro usuário

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'adbv2015',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
